ed98d7ed47d0e84067bdede96c85261bac84ebd5
turboflanagan <turboflanagan@gmail.com>
2015-11-18 21:11:30 -0500

    contacts app
    2nd version of contacts app

---
+++ ContactListViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ContactListViewController.swift
+//  2nd Contacts App
+//
+//  Created by MacBook Pro on 11/15/15.
+//  Copyright © 2015 Peter Flanagan. All rights reserved.
+//
+
+import UIKit
+
+
